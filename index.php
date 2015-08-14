f360b1e2c3740d05f1aafcf6a0b57091d5e30db2
SpiffiestOsprey <mcamwl@aol.com>
2015-08-14 11:33:33 -0700

    fixed user bug

--- index.php
+++ index.php
@@ -174,7 +174,7 @@ session_start();
         echo "0 results: Messages ";
       }
     	if ($result2->num_rows > 0 ) {
-    		$counter = 0;
+    		$counter = 1;
     		$useridArray;
     		$userusernameArray;
     		$userjoindateArray;
